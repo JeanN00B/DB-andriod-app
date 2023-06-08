5101b0ba886d5ebdd1bdc2696fb2f11bdc84f20a
Jean Camacho <jean.camacho@yachaytech.edu.ec>
2023-06-08 16:27:16 -0500

    Angular code
    Initial commit

---
+++ Form-Builder/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NameEditorComponent,
+    ProfileEditorComponent,
+    ProductFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    // other imports ...
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
